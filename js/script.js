0add0f9930b74ea9d283924aa53d8fb2512352c8
tkolsrud <mfliltim@gmail.com>
2021-11-13 17:32:09 -0800

    fetch call is retrieving results and printing to console

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log("blablabla")
+document.addEventListener("DOMContentLoaded", ()=>{
+    searchForm.addEventListener("submit", (e)=>{
+        e.preventDefault()
+        // fetch
+        const searchTerm = userInput.value
+        fetch("https://www.reddit.com/search.json?q="+searchTerm)
+        .then((responseData)=>{
+            return responseData.json()
+        })
+        .then((jsonData)=>{
+            // do whatever we need to do with the data
+            console.log(jsonData)
+        })
+        .catch((error)=>{
+            console.log(`Error retrieving data from reddit:`)
+            console.log(error)
+        })
+    })
+    console.log("The data hath loaded")
+})
